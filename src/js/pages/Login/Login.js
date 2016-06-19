6d09f221addf8da31a26b9104d19e25d4edaca49
Eric Kambestad <erickambestad@yahoo.com>
2016-06-18 21:12:27 -0700

    try to get login working

--- src/js/pages/Login/Login.js
+++ src/js/pages/Login/Login.js
@@ -1,11 +1,21 @@
 "use strict";
 
 import React from "react";
+import { attemptLogin } from '../../actions'
 
 class Login extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.login = this.login.bind(this);
+  }
+
+  login() {
+    let email = this.refs.email.value,
+      password = this.refs.password.value;
+      
+    attemptLogin(email, password);
   }
 
   render() {
@@ -13,13 +23,13 @@ class Login extends React.Component {
       <div id="login-page" className="container">
         <h1>Login</h1>
         <div>
-          <input type="text" placeholder="Email" />
+          <input type="text" ref="email" placeholder="Email" />
         </div>
         <div>
-          <input type="password" placeholder="password" />
+          <input type="password" ref="password" placeholder="password" />
         </div>
         <div>
-          <button type="submit">Submit</button>
+          <button type="submit" onClick={this.login}>Submit</button>
         </div>
       </div>
     );
--- src/js/reducers.js
+++ src/js/reducers.js
@@ -3,38 +3,37 @@
 import _ from 'lodash';
 import initialState from './initialState';
 import {
-  // ATTEMPTING_LOGIN,
-  // AWAITING_AUTH_RESPONSE,
-  // LOGOUT,
-  // ANONYMOUS,
-  // LOGIN_USER,
-  // LOGGED_IN
+  ATTEMPTING_LOGIN,
+  AWAITING_AUTH_RESPONSE,
+  LOGOUT,
+  ANONYMOUS,
+  LOGIN_USER,
+  LOGGED_IN
 } from './config';
 
 export default function(state = initialState, action) {
-  console.log(action, 'action')
   switch(action.type) {
-    // case ATTEMPTING_LOGIN:
-    //   return {
-    //       currently: AWAITING_AUTH_RESPONSE,
-    //       username: "guest",
-    //       uid: null
-    //   };
-    // break;
-    // case LOGOUT:
-    //   return {
-    //       currently: ANONYMOUS,
-    //       username: "guest",
-    //       uid: null
-    //   };
-    // break;
-    // case LOGIN_USER:
-    //   return {
-    //       currently: LOGGED_IN,
-    //       username: action.username,
-    //       uid: action.uid
-    //   };
-    // break;
+    case ATTEMPTING_LOGIN:
+      return {
+          currently: AWAITING_AUTH_RESPONSE,
+          username: "guest",
+          uid: null
+      };
+    break;
+    case LOGOUT:
+      return {
+          currently: ANONYMOUS,
+          username: "guest",
+          uid: null
+      };
+    break;
+    case LOGIN_USER:
+      return {
+          currently: LOGGED_IN,
+          username: action.username,
+          uid: action.uid
+      };
+    break;
     default:
       return state;
   }
